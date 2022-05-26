b0c2593c54a20f6025f43db7ac7c0aee1c607832
Mirawara <81705342+Mirawara@users.noreply.github.com>
2022-05-26 18:57:56 +0200

    my func

--- Utils/Crypto/crypto.h
+++ Utils/Crypto/crypto.h
@@ -1,12 +1,17 @@
 #ifndef SECURE_FILE_TRANSFER_CRYPTO_H
 #define SECURE_FILE_TRANSFER_CRYPTO_H
-
+#include <tuple>
 #include <string.h>
 #include <openssl/conf.h>
 #include <openssl/evp.h>
 #include <openssl/err.h>
 #include <openssl/hmac.h>
 #include <openssl/x509.h>
+#include <iostream>
+#include <string>
+#include <stdio.h>
+#include <limits.h>
+#include <openssl/pem.h>
 
 using namespace std;
 
@@ -22,16 +27,17 @@ public:
 
     unsigned char *dh_sharedkey(EVP_PKEY *dh_params, EVP_PKEY *public_key, EVP_PKEY *private_key);//edo
 
-    unsigned char *sign(unsigned char *msg, FILE private_key, char* psw);//lore
+    unsigned char* sign(unsigned char *clear_buf, long int clear_size, string prvkey_file_name, char* psw);//lore
 
-    bool verify_sign(unsigned char *signature, EVP_PKEY *key);//lore
+    bool verify_sign(unsigned char* sgnt_buf, int *key, long int sgnt_size, unsigned char* clear_buf,long int clear_size,X509_STORE *cert);//lore
 
     bool verify_cert(X509_STORE *cert);//lore
 
-    EVP_PKEY *extract_pubkey(X509_STORE *cert);//lore
+    //EVP_PKEY *extract_pubkey(X509_STORE *cert);//lore
 
 private:
     void handleError(char* s);//lore
+    unsigned char * shared_key; //free it when the session is finished
 };
 
 
